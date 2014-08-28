53215b214493230748d7fadb28ee4238959902dd
jianzhou <jianzhou@codeaurora.org>
2014-08-28 01:45:05 -0400

    Setting: add app move to external storage
    Add support tp move APP to external storage.
    
    Change-Id: I9d8fef2df2f36b4d8b611d8ffe87990407b44742

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -291,7 +291,7 @@ public class InstalledAppDetails extends Fragment
     }
 
     private void initMoveButton() {
-        if (Environment.isExternalStorageEmulated()) {
+        if (!Environment.isNoEmulatedStorageExist()) {
             mMoveAppButton.setVisibility(View.INVISIBLE);
             return;
         }
